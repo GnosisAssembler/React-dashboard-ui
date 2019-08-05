7fa0b6a326ee9973b0b7295310693414ea2017f8
Panos Karyp <pan.karyp@gmail.com>
2019-08-05 09:58:03 +0300

    Create the basic grid layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import './App.css';
+import Grid from './Grid';
 
 function App() {
   return (
-    <div className="App">
-      Hello dashboard UI
-    </div>
+    <Grid />
   );
 }
 
---
+++ src/Grid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Grid.css';
+
+export default function Grid() {
+    return (
+        <div class="grid-container">
+            <header class="header"></header>
+            <aside class="sidenav"></aside>
+            <main class="main"></main>
+            <footer class="footer"></footer>
+        </div>
+    )
+}
