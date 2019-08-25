5f42aee40e92fbea88d5f5427ae99d1691d8a49d
pankaryp <pan.karyp@gmail.com>
2019-08-25 21:57:37 +0300

    Add @nivo/calendar in Mast component

--- client/src/components/sections/Mast.js
+++ client/src/components/sections/Mast.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 
+//? Nivo calendar
+import { ResponsiveCalendar } from '@nivo/calendar'
+
 export default class Mast extends Component {
     constructor(props) {
         super(props);
@@ -12,6 +15,23 @@ export default class Mast extends Component {
           checkedForDate: false,
           counterDate: ""
         }   
+
+        //todo populate date
+        //todo fix the string format of date
+        this.data = [
+            {
+                "day": "2019-08-23",
+                "value": 62
+            },
+            {
+                "day": "2019-08-24",
+                "value": 62
+            },
+            {
+                "day": "2019-08-25",
+                "value": 62
+            }
+          ];
     }
 
     // handle mast
@@ -19,7 +39,19 @@ export default class Mast extends Component {
         e.preventDefault();
 
         let date = new Date().toDateString();
-        
+
+        // Post mast entry to database
+        axios.post('http://localhost:5000/api/masts', {
+            counter: 1, //default
+            date: date
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
         if(this.state.checked && this.state.checkedForDate) {
             this.setState(() => {
                 return {
@@ -58,6 +90,35 @@ export default class Mast extends Component {
                         name="mast"/>
                     <button>Add mast</button>
                 </form>
+
+                <div style={{height:'200px'}}>
+
+                <ResponsiveCalendar
+                    data={this.data}
+                    from="2019-08-23"
+                    to="2019-09-07"
+                    emptyColor="#eeeeee"
+                    colors={[ '#61cdbb', '#97e3d5', '#e8c1a0', '#f47560' ]}
+                    margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
+                    yearSpacing={40}
+                    monthBorderColor="#ffffff"
+                    dayBorderWidth={2}
+                    dayBorderColor="#ffffff"
+                    legends={[
+                        {
+                            anchor: 'bottom-right',
+                            direction: 'row',
+                            translateY: 36,
+                            itemCount: 4,
+                            itemWidth: 42,
+                            itemHeight: 36,
+                            itemsSpacing: 14,
+                            itemDirection: 'right-to-left'
+                        }
+                    ]}
+                />
+
+                </div>
             </div>
         )
     }
